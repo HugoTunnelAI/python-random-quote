c822fea71d78a0a3833141f2f1321a109994416b
HugoTunnelAI <hugo-84@sapo.pt>
2020-01-05 17:52:57 +0000

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def hugo():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  hugo()
